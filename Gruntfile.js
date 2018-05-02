f28b5cd7dc11bf9e66f8ef4e388c1d9baf7f7142
Renan Roberto <renan.silva@profite.com.br>
2018-05-02 16:21:34 -0300

    remove scoping function from gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,95 +1,91 @@
-(function() {
+'use strict'
 
-    'use strict';
+const middlewares = require('./middleware')
+const url = require('url')
+const proxy = require('proxy-middleware')
+const httpPlease = require('connect-http-please')
+const serveStatic = require('serve-static')
+require('dotenv').config()
 
-    const middlewares = require('./middleware');
-    const url = require('url');
-    const proxy = require('proxy-middleware');
-    const httpPlease = require('connect-http-please');
-    const serveStatic = require('serve-static');
-    require('dotenv').config()
+module.exports = function(grunt) {
 
-    module.exports = function(grunt) {
+    const project = process.env.PROJECT
+    const accountName = process.env.VTEX_ACCOUNT
+    const environment = process.env.VTEX_ENV
+    const secureUrl = (process.env.SECURE_URL === 'secure')
 
-        const project = process.env.PROJECT
-        const accountName = process.env.VTEX_ACCOUNT
-        const environment = process.env.VTEX_ENV
-        const secureUrl = (process.env.SECURE_URL === 'secure')
+    const verbose = grunt.option('verbose')
 
-        const verbose = grunt.option('verbose');
+    const protocol = secureUrl ? 'https' : 'http'
 
-        const protocol = secureUrl ? 'https' : 'http'
+    const imgProxyOptions = url.parse(`${protocol}://${accountName}.vteximg.com.br/arquivos`)
+    imgProxyOptions.route = '/arquivos'
 
-        const imgProxyOptions = url.parse(`${protocol}://${accountName}.vteximg.com.br/arquivos`)
-        imgProxyOptions.route = '/arquivos'
+    const portalHost = `${accountName}.${environment}.com.br`
 
-        const portalHost = `${accountName}.${environment}.com.br`
+    const portalProxyOptions = url.parse(`${protocol}://${portalHost}`)
+    portalProxyOptions.preserveHost = true
 
-        const portalProxyOptions = url.parse(`${protocol}://${portalHost}`);
-        portalProxyOptions.preserveHost = true
+    const rewriteLocation = (location) => location.replace('https:', 'http:').replace(environment, 'vtexlocal')
 
-        const rewriteLocation = (location) => location.replace('https:', 'http:').replace(environment, 'vtexlocal');
+    const errorHandler = (err, req, res, next) => {
+        let errString, _ref, _ref1;
+        errString = (_ref = (_ref1 = err.code) !== null ? _ref1.red : void 0) !== null ? _ref : err.toString().red
+        return grunt.log.warn(errString, req.url.yellow)
+    };
 
-        const errorHandler = (err, req, res, next) => {
-            let errString, _ref, _ref1;
-            errString = (_ref = (_ref1 = err.code) !== null ? _ref1.red : void 0) !== null ? _ref : err.toString().red
-            return grunt.log.warn(errString, req.url.yellow)
-        };
-
-        const config = {
-            fileName: project,
-            connect: {
-                http: {
-                    options: {
-                        hostname: "*",
-                        livereload: true,
-                        port: process.env.PORT || 80,
-                        middleware: [
-                            middlewares.disableCompression,
-                            middlewares.rewriteLocationHeader(rewriteLocation),
-                            middlewares.replaceHost(portalHost),
-                            middlewares.replaceHtmlBody(environment, accountName, secureUrl),
-                            httpPlease({
-                                host: portalHost,
-                                verbose: verbose
-                            }),
-                            serveStatic('./dist'),
-                            proxy(imgProxyOptions),
-                            proxy(portalProxyOptions),
-                            middlewares.errorHandler
-                        ]
-                    }
-                }
-            },
-            watch: {
+    const config = {
+        fileName: project,
+        connect: {
+            http: {
                 options: {
-                    livereload: true
-                },
-                dist: {
-                    files: ['dist/**/*']
-                },
-                grunt: {
-                    files: ['Gruntfile.js']
+                    hostname: "*",
+                    livereload: true,
+                    port: process.env.PORT || 80,
+                    middleware: [
+                        middlewares.disableCompression,
+                        middlewares.rewriteLocationHeader(rewriteLocation),
+                        middlewares.replaceHost(portalHost),
+                        middlewares.replaceHtmlBody(environment, accountName, secureUrl),
+                        httpPlease({
+                            host: portalHost,
+                            verbose: verbose
+                        }),
+                        serveStatic('./dist'),
+                        proxy(imgProxyOptions),
+                        proxy(portalProxyOptions),
+                        middlewares.errorHandler
+                    ]
                 }
             }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            dist: {
+                files: ['dist/**/*']
+            },
+            grunt: {
+                files: ['Gruntfile.js']
+            }
         }
-
-        const tasks = {
-            devoff: ['watch'],
-            default: ['connect', 'watch']
-        }
-
-        grunt.initConfig(config)
-
-        grunt.loadNpmTasks('grunt-contrib-connect')
-        grunt.loadNpmTasks('grunt-contrib-watch')
-
-        const results = []
-        for (let key in tasks) {
-          results.push(grunt.registerTask(key, tasks[key]));
-        }
-
-        return results
     }
 
-}).call(this)
+    const tasks = {
+        devoff: ['watch'],
+        default: ['connect', 'watch']
+    }
+
+    grunt.initConfig(config)
+
+    grunt.loadNpmTasks('grunt-contrib-connect')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+
+    const results = []
+    for (let key in tasks) {
+      results.push(grunt.registerTask(key, tasks[key]))
+    }
+
+    return results
+}
