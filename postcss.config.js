e1f5e934cc3a393d5ee00db7536b07077c8b4377
Renan Roberto <renan.silva@profite.com.br>
2018-05-03 10:35:44 -0300

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('precss'),
+    process.env.NODE_ENV === 'production' ? require('cssnano') : '',
     require('postcss-import'),
     require('postcss-cssnext')({
       features: {
